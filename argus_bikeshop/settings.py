0a5a901fcc68841a363d9788e12923c7e5535984
reinelgiro123 <rci.bsis.competentegiro@gmail.com>
2026-01-30 02:55:38 +0800

    Fixsss

--- argus_bikeshop/settings.py
+++ argus_bikeshop/settings.py
@@ -67,9 +67,9 @@ DATABASES = {
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('dffp0qybt'),
+    'API_KEY': os.environ.get('969847296223693'),
+    'API_SECRET': os.environ.get('VaHWauzWAdy6-TnzfwEcxILP2Ew'),
 }
 
 # New Django 4.2+ Storage Configuration
