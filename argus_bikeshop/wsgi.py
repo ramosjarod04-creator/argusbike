c9f78452636521769493c596678df71304767452
ramodjarod <ramosjarod04@gmail.com>
2026-02-13 08:47:21 +0800

    Update wsgi.py

--- argus_bikeshop/wsgi.py
+++ argus_bikeshop/wsgi.py
@@ -4,6 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'argus_bikeshop.settings')
 
 application = get_wsgi_application()
-
-# Vercel needs the 'app' variable to point to the WSGI application
 app = application
